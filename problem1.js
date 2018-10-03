60152c0ce5eec85f1a840f5dfab51c0784aed214
Florian Edelmaier <florian.edelmaier@gmail.com>
2018-10-03 14:36:21 +0200

    refactor

--- problem1.js
+++ problem1.js
@@ -1,50 +1,24 @@
+'use strict';
 /*
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-// var sumUpFilteredInRangeOfN = function(n, predicate) {
-//     'use strict';
-//     var n = n || 1;
-//     var i = 0, sum = 0;
-//     for (i = 0; i < n; i++) {
-//         if (predicate(i)) {
-//             sum += 1;
-//         }
-//     }
-//     return sum;
-// }
-
-var range = function(n, predicate) {
-    'use strict';
-    var i = 0;
-    var result = [];
-
-    if (!n instanceof Number) return result;
-    var predicate = predicate || function(i) { return i; };
-
-    for(i; i < n; i++) {
-      if(predicate(i)) result.push(i);
-    }
-    return result;
+const range = (n, predicate) => {
+    if(!n instanceof Number) return [];
+    const predicate = predicate || i => i;
+    
+    return [...Array(n).keys()].filter(i => predicate(i));
 }
-
-var isMultipleOf3Or5 = function(i) {
-    'use strict';
-    if (!i instanceof Number) {
-        return false;
-    }
+const isMultipleOf3Or5 = i => {
+    if (!i instanceof Number) return false;
     return !(i % 3) || !(i % 5);
 }
 
-var problem1 = function(n) {
-    'use strict';
-    var i = 0;
-
+const problem1 = function(n) {
     if (!n instanceof Number) return 0;
     return range(n, isMultipleOf3Or5).reduce(function(previous, current) { return previous + current; });
 }
 
-var result = problem1(1000);
-console.log(result);
+console.log(problem1(1000));
