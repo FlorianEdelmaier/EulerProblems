72115b35039b4a1e5bb917515874cbaacb051384
Florian Edelmaier <florian.edelmaier@gmail.com>
2018-11-28 23:44:01 +0100

    solved p9

--- problem10.js
+++ problem10.js
@@ -3,3 +3,36 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 */
+
+const isPrime = x => {
+    for(let i = 2; i < x; i++) {
+        if(x % i === 0) return false;
+    }
+    return x !== 0 && x !== 1;
+}
+
+function *primeGenerator() {
+    let nr = 0;
+    while(true) {
+        if(isPrime(nr)) yield nr;
+        nr++;
+    }
+}
+
+const problem10 = x => {
+    let result = 0;
+    const pgen = primeGenerator();
+    while(true) {
+        let prime = pgen.next().value; 
+    }
+    return result;
+}
+
+// const nthPrimeNumber = nr => {
+//     const pgen = primeGenerator();
+//     let result = [];
+//     [...Array(nr).keys()].map(i => result.push({counter: i+1, value: pgen.next().value}));
+//     return result;
+// }
+
+// console.log(nthPrimeNumber(10001).pop())
--- problem9.js
+++ problem9.js
@@ -18,17 +18,17 @@ const isPythagorasCompliant = (v1, v2, v3) => v1 ** 2 + v2 ** 2 === v3 ** 2;
 
 const problem9 = limiter => {
     const matrix = [];
-    for(let x=0; x < (limiter + 1); x++) {
-        for(let y=0; y < (limiter + 1 - x); y++) {
-            for(let z=0; z < (limiter + 1 - y); z++) {
-                if(sumIsEqualTo(1000)(x,y,z) &&
+	range(limiter+1).forEach(x => {
+		range(limiter+1-x).forEach(y => {
+			range(limiter+1-y).forEach(z => {
+				if(sumIsEqualTo(1000)(x,y,z) &&
 				   sizeIsAscending(x,y,z) &&
 				   isPythagorasCompliant(x,y,z)) {
-                    matrix.push([x,y,z])
-                }
-            }
-        }
-    }
+					matrix.push([x,y,z])
+				}
+			})
+		})
+	})
     return matrix;
 }
 
