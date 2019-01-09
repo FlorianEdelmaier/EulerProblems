2b229742a5644c096ec82f6590713822f5ce50fd
Florian Edelmaier <florian.edelmaier@gmail.com>
2019-01-10 00:47:56 +0100

    prepare p14

---
+++ problem14.js
@@ -0,0 +1,43 @@
+/*
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+*/
+
+const evenCollatz = x => x/2;
+
+const oddCollatz = x => 3 * x + 1;
+
+const isEven = x => x % 2 === 0;
+
+const getNextCollatz = x => isEven(x) ? evenCollatz(x): oddCollatz(x);
+
+const getCollatzSequence = x => {
+	let nr = x;
+	const result = [];
+	while(nr > 1) {
+		nr = getNextCollatz(nr);
+		result.push(nr);
+	}
+	return result;
+}
+
+const calcCollatzForNumbersUpTo = num => {
+	for(let i = 0; i < num; i++) {
+
+
+const problem14 = () => {
+	console.log(13, getCollatzSequence(13));
+}
+
+problem14();
