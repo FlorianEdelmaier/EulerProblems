fec685bc6bc076ec79a1d6638b1aa793fc5828d2
Florian Edelmaier <florian.edelmaier@gmail.com>
2019-01-23 17:49:14 +0100

    elab on 17

--- problem17.js
+++ problem17.js
@@ -1,5 +1,6 @@
 /*
 If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
 If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
@@ -11,7 +12,7 @@ const range = (n, predicate = i => i) => {
 
 const singles = ['zero','one','two','three','four','five','six','seven','eight','nine'];
 const tens = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-var tenners = ['ten', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+const tenners = ['ten', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
 
 const isOneDigitNumber = nStr => nStr.length === 1;
 const isTwoDigitNumber = nStr => nStr.length === 2;
@@ -19,25 +20,31 @@ const isThreeDigitNumber = nStr => nStr.length === 3;
 
 const getSingleNumber = n => singles[n];
 const getTwoDigitNumber = nAsString => {
-    console.log("two digit", nAsString)
+    //console.log(nAsString);
     if(nAsString[0] === '1') return tens[parseInt(nAsString[1])];
     else return tenners[parseInt(nAsString[0] - 1)] + (parseInt(nAsString[1]) === 0 ? '' : '-' + singles[parseInt(nAsString[1])]);
+}
+const getThreeDigitNumber = nAsString => {
+    const lastTwoDigits = (nAsString[1] === '0') ?
+            (nAsString[2] === '0') ? '' : ' and ' + getSingleNumber(nAsString[2]) : 
+            ' and ' + getTwoDigitNumber(nAsString[1] + nAsString[2]);
+    return `${singles[parseInt(nAsString[0])]} hundred${lastTwoDigits}` 
 }
 
 const number2word = n => {
     const nAsString = n.toString();
     if(isOneDigitNumber(nAsString)) return getSingleNumber(n);
     else if(isTwoDigitNumber(nAsString)) {
-        getTwoDigitNumber(nAsString);
+        return getTwoDigitNumber(nAsString);
     }
     else if(isThreeDigitNumber(nAsString)) {
-
+        return getThreeDigitNumber(nAsString)
     }
-    //else if(nAsString.length === 2 && nAsString[0] === '1') return  tens[parseInt(nAsString[0])];
-    //else if(nAsString.length === 2 && nAsString[0] > 1) return tenners[parseInt(nAsString[0]-1)] + (parseInt(nAsString[1]) === 0 ? '' : ' ' + singles[parseInt(nAsString[1])]);
+    else if(is1Thousand(nAsString)) {
+        return "one thousand";
+    }
     else throw Error("number not supported")
 }
 
-const words = range(15).map(n => number2word(n))
-
-console.log(words);
+const words = range(350).map(n => number2word(n))
+console.log(words.slice(199,303));
