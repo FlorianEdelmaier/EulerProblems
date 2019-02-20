ad0a2d7b1d27b793ba42dea9c937a8eb33a84a8b
Florian Edelmaier <florian.edelmaier@gmail.com>
2019-02-20 17:36:06 +0100

    p25

--- problem25.js
+++ problem25.js
@@ -20,16 +20,23 @@ The 12th term, F12, is the first term to contain three digits.
 
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 */
-
+const Big = require("big.js");
 
 function *fib(stop = i => false) {
     yield 0;
-    let last = 0;
-    let next = 1;
-    while(stop(last + next)) {
-        [last, next] = [next, last + next];
+    let last = new Big(0);
+    let next = new Big(1);
+    while(stop(last.plus(next))) {
+        [last, next] = [next, last.plus(next)];
         yield next;
     }
 }
 
-const isLessThan = max => i => i < max;
+const numberOfDigitsSmallerThan1000 = nr => nr.toFixed(0).toString().split('').length <= 1000
+
+let maxFib = undefined;
+for(let f of fib(numberOfDigitsSmallerThan1000)) {
+    maxFib = f;
+}
+
+console.log(maxFib.toFixed(0));
